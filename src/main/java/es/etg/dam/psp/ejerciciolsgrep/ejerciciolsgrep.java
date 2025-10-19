e5c5f872372e65989e9dbdb75c4c9295bf99beb3
AngelDRio <angeldelrio1802@gmail.com>
2025-10-19 17:07:06 +0200

    código terminado y funcional en Linux

--- src/main/java/es/etg/dam/psp/ejerciciolsgrep/ejerciciolsgrep.java
+++ src/main/java/es/etg/dam/psp/ejerciciolsgrep/ejerciciolsgrep.java
@@ -9,42 +9,41 @@ import java.io.PrintWriter;
 public class ejerciciolsgrep {
 
     // Constantes para los comandos
-    private static final String RUTA_DIRECTORIO = "archivosParaLeer"; // carpeta dentro del proyecto
+    private static final String RUTA_DIRECTORIO = "archivosParaLeer";
     private static final String COMANDO_LS = "ls " + RUTA_DIRECTORIO;
-    private static final String[] COMANDO_GREP = {"grep", "-i", "a"}; // -i = insensible mayúsculas
+    private static final String[] COMANDO_GREP = {"grep", "-i", "a"};
 
     // Mensajes
     private static final String MENSAJE_TITULO = "Archivos que contienen la letra 'a':";
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Process procesoLS = Runtime.getRuntime().exec(COMANDO_LS);
-        Process procesoGREP = Runtime.getRuntime().exec(COMANDO_GREP);
+        Process procesoLs = Runtime.getRuntime().exec(COMANDO_LS);
+        Process procesoGrep = Runtime.getRuntime().exec(COMANDO_GREP);
 
-        enviarSalidaDeLsAGrep(procesoLS, procesoGREP);
+        enviarSalidaDeLsAGrep(procesoLs, procesoGrep);
 
-        String resultado = leerSalidaDeGrep(procesoGREP);
+        String resultado = leerSalidaDeGrep(procesoGrep);
 
         System.out.println(MENSAJE_TITULO);
         System.out.println(resultado);
     }
 
     // Método que envía la salida de LS a la entrada de GREP
-    private static void enviarSalidaDeLsAGrep(Process procesoLS, Process procesoGREP) throws IOException {
-        BufferedReader lectorLS = new BufferedReader(new InputStreamReader(procesoLS.getInputStream()));
-        PrintWriter escritorGREP = new PrintWriter(new OutputStreamWriter(procesoGREP.getOutputStream()));
+    private static void enviarSalidaDeLsAGrep(Process procesoLs, Process procesoGrep) throws IOException {
+        BufferedReader lectorLS = new BufferedReader(new InputStreamReader(procesoLs.getInputStream()));
+        PrintWriter escritorGrep = new PrintWriter(new OutputStreamWriter(procesoGrep.getOutputStream()));
 
         String linea;
         while ((linea = lectorLS.readLine()) != null) {
-            escritorGREP.println(linea);
+            escritorGrep.println(linea);
         }
-
-        escritorGREP.close(); // fuerza EOF para que grep termine
+        escritorGrep.close();
     }
 
     // Método que lee la salida de GREP
-    private static String leerSalidaDeGrep(Process procesoGREP) throws IOException {
+    private static String leerSalidaDeGrep(Process procesoGrep) throws IOException {
         StringBuilder salida = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(procesoGREP.getInputStream()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(procesoGrep.getInputStream()));
         String linea;
         while ((linea = br.readLine()) != null) {
             salida.append(linea).append("\n");
